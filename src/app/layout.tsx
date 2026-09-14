050756e3e17c0a625e6eef10265d058dbcb620d5
srn932 <srn932@gmail.com>
2026-09-14 21:35:05 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,18 +26,26 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   const address = business.address ? { "@type": "PostalAddress", streetAddress: business.address, addressLocality: business.city, addressRegion: business.state, postalCode: business.postalCode, addressCountry: business.country } : undefined;
   return (
     <html lang="en">
-      <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NJ7TMB8F');</script>
-<!-- End Google Tag Manager -->
-      <body>
-        <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJ7TMB8F"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+<Script id="google-tag-manager" strategy="beforeInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NJ7TMB8F');
+      `}
+    </Script>
+
+    <body>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NJ7TMB8F"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
         <SEOJsonLd data={{ "@context": "https://schema.org", "@type": ["LocalBusiness", "PetStore"], name: business.name, description: siteConfig.description, url: siteConfig.url, telephone: business.phone || undefined, email: business.email || undefined, address, geo: business.latitude && business.longitude ? { "@type": "GeoCoordinates", latitude: business.latitude, longitude: business.longitude } : undefined, openingHours: business.openingHours.length ? business.openingHours : undefined, sameAs: [business.instagram, business.facebook, business.youtube].filter(Boolean) }} /><Navbar /><main>{children}</main><Footer /><WhatsAppButton /><ChatWidget /><MobileActionBar /></body>
     </html>
   );
