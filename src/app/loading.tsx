3a754e7a662f3d13b2520e62f7280de410d5f6b8
unknown <srn932@gmail.com>
2026-09-15 02:08:54 +0530

    Prevent initial-page layout shift

--- src/app/loading.tsx
+++
@@ -1,3 +0,0 @@
-import { Mark } from "@/components/common/logo";
-
-export default function Loading(){return <div className="flex min-h-[70vh] items-center justify-center"><div className="text-center"><Mark className="loader-paw mx-auto h-16 w-16 text-forest"/><p className="mt-4 text-[10px] font-extrabold uppercase tracking-[.18em] text-muted">Finding the way</p></div></div>}
